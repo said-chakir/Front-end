2c2eba1022204b627ff2eb7121d74f305a26ddfc
Said Chakir <saidsaidchakir@gmail.com>
2024-06-03 19:00:05 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CustomersComponent } from './customers/customers.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import { NewCustomerComponent } from './new-customer/new-customer.component';
+import { CustomerAccountComponent } from './customer-account/customer-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CustomersComponent,
+    AccountsComponent,
+    NewCustomerComponent,
+    CustomerAccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/accounts.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {AccountDetails} from "../model/account.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountsService {
+
+
+  constructor( private http : HttpClient) { }
+  public  getAccount(accountId : string , page :number , size: number ):Observable<AccountDetails>{
+    return this.http.get<AccountDetails>( "http://localhost:8085/accounts/" + accountId + "/pageOperations?page=" + page + "&size=" + size);
+}
+  public  debit(accountId : string , amount : number , description: string){
+    let data ={accountId : accountId , amount : amount , description : description}
+    return this.http.post<AccountDetails>( "http://localhost:8085/accounts/debit",data);
+  }
+  public  credit(accountId : string , amount : number , description: string){
+    let data ={accountId : accountId , amount : amount , description : description}
+    return this.http.post<AccountDetails>( "http://localhost:8085/accounts/credit",data);
+  }
+  public  transfer(accountSource : string , accountDestination : string ,amount : number, description : string){
+    let data ={accountSource,accountDestination,amount,description}
+    return this.http.post<AccountDetails>( "http://localhost:8085/accounts/transfer",data);
+  }
+}
